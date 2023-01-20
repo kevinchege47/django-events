c1018133d0826ff436b14b96b5d5bdfdc1eac762
kevinchege47 <chegekevin001@gmail.com>
2023-01-20 16:32:29 +0300

    Created base template, created navbar

--- myclub_website/app/views.py
+++ myclub_website/app/views.py
@@ -3,7 +3,7 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 
-def home(request,year,month):
+def home(request,year=datetime.now().year,month=datetime.now().strftime('%B')):
     name = "John"
     month = month.title()
     month_number = list(calendar.month_name).index(month)
@@ -13,7 +13,7 @@ def home(request,year,month):
     cal = HTMLCalendar().formatmonth(year,month_number)
     now = datetime.now()
     current_year = now.year
-    time = now.strftime('%I:%M:%S %p')
+    time = now.strftime('%H:%M: %p')
     context={"name":name,"year":year,"month":month,"cal":cal,"current_year":current_year,"time":time}
-    return render(request,'home.html',context)
+    return render(request,'app/home.html',context)
 # Create your views here.
