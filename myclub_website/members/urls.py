62ee2f3577ca831b17735ee19a57a886504ddec7
kevinchege47 <chegekevin001@gmail.com>
2023-01-25 12:09:45 +0300

    Regular User Registration

--- myclub_website/members/urls.py
+++ myclub_website/members/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login_user',views.login_user,name="login-user"),
-    path('logout_user',views.logout_user,name="logout-user")
+    path('logout_user',views.logout_user,name="logout-user"),
+    path('register_user',views.register_user,name="register-user")
 ]
